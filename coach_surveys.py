6278c78e04668faac98fb0e532383ec082f5eb14
Anna Mouland <anna.electros@gmail.com>
2022-01-15 12:00:45 -0500

    added exception handling

--- coach_surveys.py
+++ coach_surveys.py
@@ -3,7 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-data = pd.read_csv(sys.argv[2], usecols=list(range(2, 3)) + list(range(4, 16)))
+if len(sys.argv) != 3:
+    print("ERROR: Too many arguments passed to script.\n"
+          "Please call this script followed by the coach name and the csv file path.\n"
+          "Here is an example call:\n"
+          "python3 coach_surveys.py \"Jonathan Stanhope\" ./survey_responses.csv")
+    sys.exit(0)
+
+try:
+    data = pd.read_csv(sys.argv[2], usecols=list(range(2, 3)) + list(range(4, 16)))
+except FileNotFoundError as err:
+    print(err)
+    print(sys.argv[2], "does not exist in the directory indicated by the path you provided.\n"
+          "Please check for typos. If there are none, please copy the csv file's pathname from\n"
+          "the root and pass it as your csv argument:\n"
+          "python3 coach_surveys.py \"Jonathan Stanhope\" /Users/bob/Documents/TalkMaze/survey_data.csv")
+    sys.exit(0)
+except pd.errors.EmptyDataError:
+    print(sys.argv[2], "was empty. Please check your csv and make sure it contains data.")
+    sys.exit(0)
 
 x = data.loc[data['What is your coach\'s name?'] == sys.argv[1]]
 
@@ -19,6 +37,10 @@ for r in dict_copy:
     else:
         new_dict = dict_copy.get(r)
         key_arr = list(new_dict.keys())
+        if len(key_arr) == 0:
+            print("ERROR: Coach not found.\n"
+                  "Please check for typos in the coach's name provided and review your data.")
+            sys.exit(0)
 
         for i in range(key_arr[0], key_arr[-1]+1):
             value = new_dict.get(i)
